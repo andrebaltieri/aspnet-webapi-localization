309bc8ee27e7f1f0ca3ed1da06c31b7a107c127f
Andre Baltieri <andrebaltieri@hotmail.com>
2021-10-19 08:34:45 -0300

    Finalizado a demo

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using LocalizaMe.Localize;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace LocalizaMe.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("/")]
+        public IActionResult Index(
+            [FromServices] IStringLocalizer<Messages> localizer)
+        {
+            //return Ok(_resourceManager.GetString("Hello World"));
+            return Ok(new
+            {
+                Message = localizer["Hello World"].Value,
+                Date = DateTime.Now,
+                DateUtc = DateTime.Now.ToUniversalTime()
+            });
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Localization.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+
+namespace LocalizaMe
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLocalization();
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            var supportedCultures = new[] { "pt-BR", "en-US", "it" };
+            var localizationOptions = new RequestLocalizationOptions()
+                .SetDefaultCulture(supportedCultures[0])
+                .AddSupportedCultures(supportedCultures)
+                .AddSupportedUICultures(supportedCultures);
+
+            app.UseRequestLocalization(localizationOptions);
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapControllerRoute("default", "{culture:culture}/{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
